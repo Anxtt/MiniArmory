801403fe7796aaf8318d408ed384f83a995d95e7
Anxiety <elmagic11@abv.bg>
2022-03-30 04:15:55 +0300

    small tweaks and authentication

--- MiniArmory.Web/Controllers/FactionController.cs
+++ MiniArmory.Web/Controllers/FactionController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using MiniArmory.Core.Models;
 using MiniArmory.Core.Services.Contracts;
 
@@ -11,10 +12,12 @@ namespace MiniArmory.Web.Controllers
         public FactionController(IFactionService factionService) 
             => this.factionService = factionService;
 
+        [Authorize(Roles = "Owner")]
         public IActionResult AddFaction()
             => this.View();
 
         [HttpPost]
+        [Authorize(Roles = "Owner")]
         public async Task<IActionResult> AddFaction(FactionFormModel model)
         {
             if (!ModelState.IsValid)
