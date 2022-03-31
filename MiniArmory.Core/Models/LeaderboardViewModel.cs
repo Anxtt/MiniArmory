11ad1a3a0c67fa77a8892f1d329591bec7da1406
Anxiety <elmagic11@abv.bg>
2022-03-31 04:43:13 +0300

    Character logic and views

---
+++ MiniArmory.Core/Models/LeaderboardViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniArmory.Core.Models
+{
+    public class LeaderboardViewModel
+    {
+        [Range(1, int.MaxValue)]
+        public int Rank { get; set; }
+
+        public short Rating { get; set; }
+
+        public string Name { get; set; }
+
+        public string Realm { get; set; }
+
+        public string Class { get; set; }
+
+        public string Faction { get; set; }
+
+        public short Win { get; set; }
+
+        public short Loss { get; set; }
+    }
+}
---
+++ MiniArmory.Core/Services/CharacterService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniArmory.Core.Models;
+using MiniArmory.Core.Services.Contracts;
+using MiniArmory.Data.Data;
+using MiniArmory.Data.Data.Models;
+
+namespace MiniArmory.Core.Services
+{
+    public class CharacterService : ICharacterService
+    {
+        private readonly MiniArmoryDbContext db;
+
+        public CharacterService(MiniArmoryDbContext db)
+            => this.db = db;
+
+        public async Task Add(CharacterFormModel model, Guid id)
+        {
+            Character character = new Character()
+            {
+                Name = model.Name,
+                RealmId = model.Realm,
+                FactionId = model.Faction,
+                RaceId = model.Race,
+                ClassId = model.Class,
+                UserId = id
+            };
+
+            await this.db.Characters.AddAsync(character);
+            await this.db.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<JsonFormModel>> GetRealms()
+            => await this.db
+            .Realms
+            .Select(x => new JsonFormModel()
+            {
+                Id = x.Id,
+                Name = x.Name,
+            })
+            .ToListAsync();
+
+        public async Task<IEnumerable<LeaderboardViewModel>> LeaderboardStats()
+            => await this.db
+            .Characters
+            .Include(x => x.Faction)
+            .Include(x => x.Class)
+            .Include(x => x.Realm)
+            .Select(x => new LeaderboardViewModel()
+            {
+                Class = x.Class.Image,
+                Faction = x.Faction.Image,
+                Name = x.Name,
+                Loss = x.Loss,
+                Realm = x.Realm.Name,
+                Rating = x.Rating,
+                Win = x.Win
+            })
+            .ToListAsync();
+    }
+}
