689f9fca350e8e5aef099709552e61741645351f
Anxiety <elmagic11@abv.bg>
2022-04-06 04:57:04 +0300

    Add role Memeber upon registration

--- MiniArmory.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MiniArmory.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -135,6 +135,8 @@ namespace MiniArmory.Web.Areas.Identity.Pages.Account
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
+                    await _userManager.AddToRoleAsync(user, "Member");
+
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
                         return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
